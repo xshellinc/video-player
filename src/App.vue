5a29ea88277ccc8b7406384b4630a1b5cb53b976
daryl <darylcoleoffice@gmail.com>
2018-08-28 14:11:35 +0900

    update video file source

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 			controls
 			id="video"
 			type="video/mp4"
-			src="./assets/big_buck_bunny_720p_surround.mp4"
+			src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
 			poster="https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217"
 			width="620" 
 		/>
